5f657a3d11c02f55be9eb4aab5c35e8cfbe8f2e8
Dmitriy <aua008@gmail.com>
2024-02-21 16:58:42 +0200

    added verifying form data on client and added accessibility

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,9 +20,7 @@ export async function fetchRevenue() {
 
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
     console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
@@ -167,7 +165,6 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
-    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
